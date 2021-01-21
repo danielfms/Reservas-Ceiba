c29719e9a4db86e04fc06eba708e49accb5a123c
Daniel Felipe Marín Sánchez - Ceiba Software <daniel.marin@ceiba.com.co>
2021-01-21 12:52:02 -0500

    add logger a servicio consultar reserva

--- reservas-backend/microservicio/dominio/src/main/java/com/ceiba/reserva/servicio/ServicioConsultarReserva.java
+++ reservas-backend/microservicio/dominio/src/main/java/com/ceiba/reserva/servicio/ServicioConsultarReserva.java
@@ -1,11 +1,14 @@
 package com.ceiba.reserva.servicio;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import com.ceiba.dominio.excepcion.ExcepcionSinDatos;
 import com.ceiba.usuario.modelo.dto.DtoReserva;
 import com.ceiba.usuario.puerto.dao.DaoReserva;
 
 public class ServicioConsultarReserva {
 
+	private static final Logger LOGGER_ERROR = LoggerFactory.getLogger(ServicioConsultarReserva.class);
 	private final DaoReserva daoReserva;
 
     public ServicioConsultarReserva(DaoReserva daoReserva) {
@@ -15,6 +18,7 @@ public class ServicioConsultarReserva {
     public DtoReserva ejecutar(Long id) {
     	DtoReserva dtoReserva = this.daoReserva.consultar(id);
     	if(dtoReserva == null){
+    		LOGGER_ERROR.error("Registro no encontrado");
     		throw new ExcepcionSinDatos("Registro no encontrado");
     	}
     	return dtoReserva;
